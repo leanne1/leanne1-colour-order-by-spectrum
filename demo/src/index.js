8acd1dc40501b7725117c6686115154c44c9f4c4
leanne1 <leanne.tite@gmail.com>
2017-01-07 17:36:14 +0000

    Debug nwb publish task

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,6 @@
 require('../../src/demo/styles/index.less');
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 import DemoPage from '../../src/demo/components/DemoPage';
 import colours from '../../src/colour-swatch';
 import { finalColourList } from '../../src'
